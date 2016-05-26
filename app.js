b4e43897a96b4fa306c257a1eed80319f2332857
Tom Ashworth <tgvashworth@gmail.com>
2016-05-26 01:43:45 +0100

    Style

--- app.js
+++ app.js
@@ -4,19 +4,40 @@ window.$ = window.jQuery = require("jquery");
 import { createStore, combineReducers } from "redux";
 import { makeConnect } from "./kite-redux";
 import { createComponent, attach } from "./kite";
+import Style from "./Style";
 
 import { h } from "virtual-dom";
 
 
 // UI
 
+const styles = Style.create({
+  button: {
+    fontSize: "1rem",
+    padding: "3em",
+    width: "100%",
+    border: "5px solid rgba(0,0,0,0.5)",
+    outlineColor: "rgba(255,255,255,0.2)",
+    outlineStyle: "solid"
+  }
+});
+const styles2 = Style.create({
+  root: {
+    padding: "3em"
+  }
+});
+
+
 var Example = createComponent({
   attributes: {
-    clicks: 0
+    clicks: 0,
+    button: "button"
   },
 
   initialize() {
-    this.on("click", this.handleClick);
+    this.on("click", {
+      button: this.handleClick
+    });
   },
 
   handleClick(e) {
@@ -26,8 +47,22 @@ var Example = createComponent({
   },
 
   render() {
-    return h("button", {}, [
-      String(this.attr.clicks)
+    const { clicks } = this.attr;
+    return h("div", {
+      className: Style.cx(styles2.root),
+      style: Style.sx({
+        backgroundColor: `hsl(${clicks * 10 + 20}, 50%, 20%)`
+      })
+    }, [
+      h("button", {
+        className: Style.cx(styles.button),
+        style: Style.sx({
+          color: `hsl(${clicks * 10}, 50%, 60%)`,
+          backgroundColor: `hsl(${clicks * 10 - 22.5}, 50%, 20%)`
+        })
+      }, [
+        `Clicks: ${this.attr.clicks}`
+      ])
     ]);
   }
 });
